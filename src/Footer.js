42de5b57f109e2022b08737fe948fb35713bc828
HoangLe <hoangle.covid@gmail.com>
2025-08-06 21:36:40 +0700

    updated 06/08 21h30p

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import { useStoreState } from "easy-peasy";
 
 const Footer = () => {
   const postCount = useStoreState((state) => state.postCount);
-  const today = new Date();
+  // const today = new Date();
   return (
     <footer className="Footer">
       <p>{postCount} Blog Posts</p>
